6bcd6c93d7dc139bac695be11cb36a9cdac045b6
Pradip <137907031+Pradip-A-Dhadbale@users.noreply.github.com>
2023-07-01 18:24:59 +0530

    Power_function.c

---
+++ power_function.c
@@ -0,0 +1,14 @@
+ /* Using power function******* */
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,y,p;
+    printf("enter x nad y integers::");
+    scanf("%d%d",&x,&y);
+    
+    p=pow(x,y);
+    printf("anss=%d",p);
+}
+}
