44fb4a50474717e8846edf407df026a867225ccd
Pradip <137907031+Pradip-A-Dhadbale@users.noreply.github.com>
2023-07-05 03:53:43 -0700

    Same_num_array.c

---
+++ same_num_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int a[20],n,i;
+	printf("Enter array limit::");
+	scanf("%d",&n);
+	printf("Enter array elemnts:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	isarray(a,n);
+}
+void isarray(int a[20],int n)
+{
+	int n1,c=0,i;
+	printf("Enter number to find:");
+	scanf("%d",&n1);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==n1)
+			c++;
+	}
+	printf("\n%d occurs %d times",n1,c);
+
+}
